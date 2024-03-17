027d5f225cd4f9a81bfeab848c585c256cf18625
Bernardino Ochoa <bernardo.ochoa@concentrix.com>
2024-03-18 03:55:30 +0800

    add more dynamic component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
 import { Montserrat } from "next/font/google";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import {
-  getAbout,
-  getFeaturedProjects,
-  getSocials,
-} from "@/sanity/sanity.utils";
-import AboutPage from "@/components/About";
+
+import { getAbout, getFeaturedProjects } from "@/sanity/sanity.utils";
+
+import dynamic from "next/dynamic";
+import Loading from "@/components/ui/Loading";
 
 const montserrat = Montserrat({
   subsets: ["latin-ext"],
+});
+
+const HeroComponent = dynamic(() => import("@/components/Hero"), {
+  loading: () => <Loading />,
+});
+const ProjectsComponent = dynamic(() => import("@/components/Projects"), {
+  loading: () => <Loading />,
+});
+const AboutPageComponent = dynamic(() => import("@/components/About"), {
+  loading: () => <Loading />,
 });
 
 export default async function Home() {
@@ -17,9 +24,9 @@ export default async function Home() {
   const about = await getAbout();
   return (
     <main className={` h-full ${montserrat.className} `}>
-      <Hero about={about} />
-      <Projects featuredProject={featuredProjects} />
-      <AboutPage about={about} />
+      <HeroComponent about={about} />
+      <ProjectsComponent featuredProject={featuredProjects} />
+      <AboutPageComponent about={about} />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,12 @@
 import { About } from "@/types/About";
-import React, { Suspense } from "react";
+import React from "react";
 import TextGenerateEffect from "./ui/TextGenerateEffect";
 import dynamic from "next/dynamic";
+import Loading from "./ui/Loading";
 
 const BoxesComponent = dynamic(() => import("./ui/BackgroundBoxes"), {
   ssr: false,
+  loading: () => <Loading />,
 });
 
 type Props = {
@@ -15,9 +17,7 @@ function Hero({ about }: Props) {
   const introduction = `Hi!👋`;
   return (
     <section className="relative  overflow-hidden  pt-10 sm:pt-16 md:pt-0 2xl:pt-0  h-screen">
-      <Suspense fallback={"Loading...."}>
-        <BoxesComponent />
-      </Suspense>
+      <BoxesComponent />
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl md:text-4xl lg:text-7xl">
         <div className=" justify-start">
           <TextGenerateEffect
